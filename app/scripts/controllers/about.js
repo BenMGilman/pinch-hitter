6a7ac16d232dcaf0515139ef6e38fa3be14ccd5e
bgilman <bgilman@Ben-Gilman-Mac.local>
2014-08-15 20:13:29 -0500

    Set up structure of app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pinchHitterApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pinchHitterApp
+ */
+angular.module('pinchHitterApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pinchHitterApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pinchHitterApp
+ */
+angular.module('pinchHitterApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
